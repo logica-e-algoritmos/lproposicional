0de3dea7f3af2f5a682d395fd1d05be096befdb8
Flávio Leonardo Cavalcanti de Moura <contato@flaviomoura.mat.br>
2021-11-20 05:51:19 -0300

    Add files via upload

---
+++ and_i_sol.v
@@ -0,0 +1,12 @@
+Variables p q: Prop.
+
+ Section landi.
+  Hypothesis H1: p.
+  Hypothesis H2: q.
+  Lemma landi: p /\ q.
+  Proof.
+    split.
+    - assumption.
+    - assumption.
+  Qed.
+End landi.
