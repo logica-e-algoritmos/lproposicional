debbfa5236186812fe3d746f9d3497ba388668f5
Flávio Leonardo Cavalcanti de Moura <contato@flaviomoura.mat.br>
2021-11-30 14:58:23 -0300

    Update and_i.v

--- and_i.v
+++ and_i.v
@@ -1,9 +1,9 @@
-Variables p q: Prop.
+Variables phi psi: Prop.
 
  Section landi.
-  Hypothesis H1: p.
-  Hypothesis H2: q.
-  Lemma landi: p /\ q.
+  Hypothesis H1: phi.
+  Hypothesis H2: psi.
+  Lemma landi: phi /\ psi.
   Proof.
     Admitted.
 End landi.
